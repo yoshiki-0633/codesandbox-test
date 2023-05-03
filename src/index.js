bd9762ec53188d85a670d32dded0b6d4e8c128ac
yoshiki-0633 <132421616+yoshiki-0633@users.noreply.github.com>
2023-05-03 14:10:38 +0900

    変数

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import "./styles.css";
+// //var変数
+// var val1 = "val1";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // varは再代入可能
+// val1 = "val1を再代入";
+// console.log(val1);
+
+// // varは再宣言可能
+// var val1 = "val1の再宣言";
+// console.log(val1);
+
+// // let変数
+// let val2 = "val2";
+// console.log(val2);
+
+// //let変数は再代入可能
+// val2 = "val2の再代入";
+// console.log(val2);
+
+// //let変数は再宣言不可能
+
+// let val2 = "val2を再宣言";
+
+// //const変数
+// const val3 = "val3";
+// console.log(val3);
+
+// //const変数は再代入も再宣言も不可能
+
+// const val4 = {
+//   name: "YOSHI",
+//   year: 22
+// };
+
+// val4.name = "兆生";
+// console.log(val4);
